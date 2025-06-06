67399222fb742ca408b481ef205ab775b27a207c
Kento.Sasaki <Sasaki.Kento@trans-cosmos.co.jp>
2025-06-06 14:45:28 +0900

    kadai-tasklist

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -39,8 +39,14 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',   
+            'content' => 'required|max:255',
+        ]);
         // タスクを作成
         $task = new Task;
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
@@ -81,9 +87,16 @@ class TasksController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10', 
+            'content' => 'required|max:255',
+        ]);
+
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // タスクを更新
+        $task->status = $request->status;    
         $task->content = $request->content;
         $task->save();
 
